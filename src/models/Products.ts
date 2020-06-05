e84fe19ab3b8266aaed5b53164beb36345015497
vicent90 <vicentemazzaro@hotmail.com>
2020-06-05 18:02:09 -0300

    Products routes

---
+++ src/models/Products.ts
@@ -0,0 +1,38 @@
+import { Schema, model } from 'mongoose';
+
+const validShops = {
+  values: ['CARNICERIA', 'CARNICERIA'],
+  message: '{VALUE} no es un negocio válido'
+};
+
+const validMeats = {
+  values: ['VACUNOS', 'AVES', 'CERDO', 'PREPARADOS', 'EMBUTIDOS'],
+  message: '{VALUE} no es un tipo de carnicería válido'
+};
+
+const validMeatsPreparation = {
+  values: ['VACUNOS', 'AVES', 'CERDO', 'PREPARADOS', 'EMBUTIDOS'],
+  message: '{VALUE} no es un tipo de carnicería válido'
+};
+
+const validUnitMeasure = {
+  values: ['UNIDAD', 'KG'],
+  message: '{VALUE} no es una unidad de medida válida'
+}
+
+const productSchema = new Schema({
+  name: { type: String, required: [true, 'El nombre del producto es necesario'] },
+  price: { type: Number },
+  description: { type: String },
+  shop: { type: String, enum: validShops, required: [true, 'Se debe elegir un negocio (' + validShops.values[0] + 'ó ' + validShops.values[1] + ')'] },
+  meats: { type: String, required: false, enum: validMeats },
+  meatsPreparation: { type: String, required: false, enum: validMeatsPreparation },
+  unitMeasure: { type: String, required: true, default: 'KG', enum: validUnitMeasure },
+  stockQuantity: { type: Number },
+  imageUrl: { type: String },
+  active: { type: Boolean, required: true, default: true },
+  createdAt: { type: Date, default: Date.now },
+  updateAt: { type: Date }
+});
+
+export default model('Product', productSchema);
---
+++ src/routes/productsRoutes.ts
@@ -0,0 +1,87 @@
+import { Request, Response, Router } from 'express';
+import mongoose from 'mongoose';
+import Product from '../models/Products';
+
+class ProductsRoutes {
+  router: Router;
+  constructor() {
+    this.router = Router();
+    this.routes();
+  }
+
+  public getProducts(req: Request, res: Response) {
+    Product.find()
+      .then(products => {
+        res.status(200).json(products);
+      })
+      .catch((err: any) => {
+        console.log(err);
+        res.json(err);
+      });
+  }
+
+  public async getProduct(req: Request, res: Response): Promise<void> {
+    console.log(req.params.url);
+    const product = await Product.findOne({ url: req.params.url });
+    res.json(product);
+  }
+
+  public createProduct(req: Request, res: Response) {
+    const { name, price, description, shop, meats, meatsPreparation,
+      unitMeasure, stockQuantity, imageUrl, active, updateAt } = req.body;
+    const newProduct = new Product({
+      name, price, description, shop, meats, meatsPreparation,
+      unitMeasure, stockQuantity, imageUrl, active, updateAt
+    });
+    newProduct
+      .save()
+      .then(productCreated => {
+        res.status(201).json({
+          message: 'Producto creado',
+          paciente: productCreated
+        })
+      })
+      .catch((err: any) => {
+        res.status(500).json({
+          message: 'Error al crear el producto',
+          errors: err
+        });
+      });
+  }
+
+  public async updateProduct(req: Request, res: Response): Promise<void> {
+    const { url } = req.params;
+    const product = await Product.findOneAndUpdate({ url }, req.body, { new: true });
+    res.send({ message: 'product updated', product });
+  }
+
+  public deteleProduct(req: Request, res: Response) {
+    const { id } = req.params;
+    Product.findByIdAndDelete(id)
+      .then((resp) => {
+        console.log(resp);
+        res.status(200).json({
+          message: 'Producto borrado correctamente'
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: 'Error borrar el prodcuto',
+          errors: err
+        });
+      })
+    res.send({ message: 'product deleted' });
+  }
+
+  routes() {
+    this.router.get('/products', this.getProducts);
+    // this.router.get('/products/:id', this.getProduct);
+    this.router.post('/products', this.createProduct);
+    // this.router.put('/products/:url', this.updateProduct);
+    this.router.delete('/products/:id', this.deteleProduct);
+  }
+}
+
+const productsRoutes = new ProductsRoutes();
+
+export default productsRoutes.router;
