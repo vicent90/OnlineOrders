2b5f7551755e4d1352df43e52410bf8eee674f90
vicent90 <vicentemazzaro@hotmail.com>
2020-06-05 00:19:12 -0300

    add conection

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
+import indexRoutes from './routes/indexRoutes';
+
 class Server {
   public app: express.Application;
 
@@ -11,11 +13,29 @@ class Server {
   }
 
   config() {
+    const MONGO_LOCAL = '';
+    mongoose.set('useFindAndModify', true);
+    mongoose.connect(process.env.MONGODB_URI || MONGO_LOCAL, {
+      useNewUrlParser: true,
+      useCreateIndex: true
+    })
+      .then(() => {
+        console.log(process.env.MONGODB_URI);
+        console.log('Base de datos conectada');
+      })
+      .catch(() => {
+        console.log(process.env.MONGODB_URI);
+        console.log('Error al conectar a db Heroku')
+      });
 
+    this.app.set('port', process.env.PORT || 3000);
+    // Middlewares
+    this.app.use(express.json()); // Replace of body parser
+    this.app.use(express.urlencoded({ extended: false }));
   }
 
   routes() {
-
+    this.app.use(indexRoutes);
   }
 
   start() {
