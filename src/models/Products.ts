dafde5a37f91c035c0a68755a8867002f4cedabf
vicent90 <vicentemazzaro@hotmail.com>
2020-07-30 10:33:44 -0300

    Product model unit weight

--- src/models/Products.ts
+++ src/models/Products.ts
@@ -23,7 +23,7 @@ const validMeatsPreparation = {
 };
 
 const validUnitMeasure = {
-  values: ['UNIDAD', 'KG', 'PIEZA', '100 Gramos'],
+  values: ['UNIDAD', 'KG', '100 Gramos'],
   message: '{VALUE} no es una unidad de medida válida'
 }
 
